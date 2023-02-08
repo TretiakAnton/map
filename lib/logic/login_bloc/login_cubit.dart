72e4f270e16dab7d2aa48e2c37d99e2224fe2f23
TretiakAnton <anton.v.tretyak@gmail.com>
2023-02-08 14:04:43 +0200

    login screen done

--- lib/logic/login_bloc/login_cubit.dart
+++ lib/logic/login_bloc/login_cubit.dart
@@ -1,8 +1,29 @@
 import 'package:bloc/bloc.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:meta/meta.dart';
 
 part 'login_state.dart';
 
 class LoginCubit extends Cubit<LoginState> {
   LoginCubit() : super(LoginInitial());
+  final googleSignIn = GoogleSignIn();
+  GoogleSignInAccount? _user;
+
+  GoogleSignInAccount get user => _user!;
+
+  Future<bool> googleLogin() async {
+    final googleUser = await googleSignIn.signIn();
+    if (googleUser == null) return false;
+    _user = googleUser;
+    final googleAuth = await googleUser.authentication;
+    final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken, idToken: googleAuth.idToken);
+    final result = await FirebaseAuth.instance.signInWithCredential(credential);
+    if (result.credential != null) {
+      return true;
+    }
+    emit(LoginCompleted());
+    return false;
+  }
 }
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,9 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:map/logic/login_bloc/login_cubit.dart';
+import 'package:map/screens/map_screen.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -8,19 +13,40 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  TextEditingController emailController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const ImageIcon(
-          AssetImage('assets/images/map_img.png'),
-        ),
-        TextFormField(
-          controller: emailController,
-        ),
-        TextFormField(),
-      ],
+    final bloc = context.read<LoginCubit>();
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          SizedBox(
+            width: double.infinity,
+            height: 200,
+            child: CachedNetworkImage(
+              imageUrl:
+                  'https://cdn.icon-icons.com/icons2/2631/PNG/512/google_maps_new_logo_icon_159147.png',
+              placeholder: (context, url) => const CircularProgressIndicator(),
+            ),
+          ),
+          OutlinedButton.icon(
+              onPressed: () async {
+                final bool isSuccess = await bloc.googleLogin();
+                if (isSuccess) {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const MapScreen()));
+                }
+              },
+              style: OutlinedButton.styleFrom(
+                minimumSize: const Size(100, 50),
+              ),
+              icon: const FaIcon(
+                FontAwesomeIcons.google,
+                color: Colors.red,
+              ),
+              label: const Text('Sign Up with Google'))
+        ],
+      ),
     );
   }
 }
